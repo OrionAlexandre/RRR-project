f34d4dac05bc60bca84c8b8da9098db3ba231a44
Alexandre T. <tchokobiyao@gmail.com>
2025-10-15 00:08:49 +0000

    Conception de la base de données en cours. Erreur avec flask dans le shell.

--- application/app.py
+++ application/app.py
@@ -2,13 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from datetime import datetime
+
 
 db = SQLAlchemy()
+
+annee = datetime.now().year
 
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./database.db"  # Création de la base de données.
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./database.db"  # Création de la base de données.
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///./database_{annee}.db"  # Création de la base de données.
 
     db.init_app(app)
 
@@ -16,5 +21,7 @@ def create_app():
     from routes import enregistrer_routes
 
     enregistrer_routes(app, db)
+
+    migrate = Migrate(app, db)
 
     return app
--- application/models.py
+++ application/models.py
@@ -1,3 +1,20 @@
 """
 Fichier de création des différents modèles de tables des bdd.
 """
+from datetime import datetime
+from app import db
+
+mois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+
+for mois_ in mois:
+    class Mois(db.model):
+        __tablename__ = f"{mois}"
+
+        nb_total_domaines = db.Column(db.Integer, default=0)
+        nb_domaines_actifs = db.Column(db.Integer, default=0)
+        nb_total_nouveaux = db.Column(db.Integer, default=0)
+        nb_domaines_resilises = db.Column(db.Integer, default=0)
+        nb_domaines_grace = db.Column(db.Integer, default=0)
+        nb_domaines_redemption = db.Column(db.Integer, default=0)
+        nb_domaines_expires = db.Column(db.Integer, default=0)
+        nb_domaines_litigieux = db.Column(db.Integer, default=0)
